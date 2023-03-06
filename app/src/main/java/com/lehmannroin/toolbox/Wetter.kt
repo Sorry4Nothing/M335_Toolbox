aa948867d2994acd1513e0bfaac3db801172421b
Sorry4Nothing <robnoleh@gmail.com>
2023-03-06 13:35:03 +0100

    added GPS localisation

--- app/src/main/java/com/lehmannroin/toolbox/Wetter.kt
+++ app/src/main/java/com/lehmannroin/toolbox/Wetter.kt
@@ -1,11 +1,87 @@
 package com.lehmannroin.toolbox
 
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 class Wetter : AppCompatActivity() {
+
+    private lateinit var locationManager: LocationManager
+    private val LOCATION_PERMISSION_REQUEST_CODE = 100
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wetter)
+        setContentView(R.layout.activity_main)
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        checkPermission()
     }
+
+    fun checkPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+        } else {
+            getLocation()
+        }
+    }
+
+    fun getLocation() {
+        try {
+            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (location != null) {
+                val latitude = location.latitude
+                val longitude = location.longitude
+            } else {
+                locationManager.requestLocationUpdates(
+                    LocationManager.GPS_PROVIDER,
+                    900000,
+                    100f,
+                    locationListener
+                )
+            }
+        } catch (e: SecurityException) {
+            e.printStackTrace()
+        }
+    }
+
+    private val locationListener: LocationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            val latitude = location.latitude
+            val longitude = location.longitude
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+
+        override fun onProviderEnabled(provider: String) {}
+
+        override fun onProviderDisabled(provider: String) {}
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLocation()
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/lehmannroin/toolbox/getGPS.kt
@@ -0,0 +1,12 @@
+package com.lehmannroin.toolbox
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class getGPS : AppCompatActivity()  {
+}
