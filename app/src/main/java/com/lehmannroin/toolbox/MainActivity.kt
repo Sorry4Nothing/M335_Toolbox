6b64d833bd350a9520b4704a6bc0f6a15d2089c3
Sorry4Nothing <robnoleh@gmail.com>
2023-03-06 09:29:08 +0100

    added logo, started with widget

--- app/src/main/java/com/lehmannroin/toolbox/MainActivity.kt
+++ app/src/main/java/com/lehmannroin/toolbox/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.lehmannroin.toolbox
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var buttonWetter: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        buttonWetter = findViewById(R.id.buttonWetter)
+
+        buttonWetter.setOnClickListener {
+            val intent = Intent(this, Wetter::class.java)
+            startActivity(intent)
+        }
+
     }
+
 }
---
+++ app/src/main/java/com/lehmannroin/toolbox/Wetter.kt
@@ -0,0 +1,11 @@
+package com.lehmannroin.toolbox
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Wetter : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_wetter)
+    }
+}
